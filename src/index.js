2e28697e5758937ae8a1b86c11bd83ed6ca2b402
Shawn Murphy <smurp@smurp.com>
2019-02-07 17:52:43 +0100

    restore demoMode with even just a root node

--- src/index.js
+++ src/index.js
@@ -1061,7 +1061,7 @@ class DiversusFlower extends Heir {
       console.log('preparing demoModeAfterNoDataSec', this.props.demoModeAfterNoDataSec);
       setTimeout(() => {
         var pCount = this.getPetalCount();
-        if (!this.getRootKey()) {
+        if (!pCount) {
           console.log('no data, so demo mode, petalCount:',pCount);
           this.startRandomStream();
         } else {
