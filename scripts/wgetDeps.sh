5f02ea6c5036f886606fc0bc29c629462a431df6
Shawn Murphy <smurp@smurp.com>
2019-02-14 08:53:45 +0100

    add wgetDeps.sh

---
+++ scripts/wgetDeps.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd demo
+wget https://cdn.jsdelivr.net/npm/mainloop.js@latest/build/mainloop.min.js
+wget https://unpkg.com/prop-types@15.6/prop-types.min.js
+wget https://unpkg.com/react@16.7/umd/react.development.js
+wget https://unpkg.com/react-dom@16.7/umd/react-dom.development.js
